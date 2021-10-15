01bca5bccf178b695c093ea84673e417063da3d0
NseckinM <seckinmantar@gmail.com>
2021-10-15 13:03:13 +0300

    list notes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,32 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import {Button, button} from 'react-bootstrap';
+import { useState , useEffect } from 'react';
+const apiRoot ="https://localhost:44381/api/notes"
 
 function App() {
+
+  const [notes, setnotes] = useState([])
+
+  useEffect(() => {
+
+    fetch(apiRoot)
+    .then(Response => Response.json())
+    .then(data => setnotes(data));
+
+  }, [notes]);
+
   return (
     <div className="container mt-3">
-      <h1>RNote</h1>
-      <button className="btn btn-primary">Bir Buton</button>
-      <Button variant="danger">Bir Buton</Button>
+      <h1>My Notes</h1>
+
+      <ul>
+        {notes.map(x =>
+
+          <li>{x.title}</li>
+
+        )}
+      </ul>
+
     </div>
   );
 }
